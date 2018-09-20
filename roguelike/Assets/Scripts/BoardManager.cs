1547fb70502ea7d92e636978d9174784c2c2d93b
Lukas <lukashtxb16@gmail.com>
2018-09-20 13:09:55 +0200

    opdatering kode

--- roguelike/Assets/Scripts/BoardManager.cs
+++ roguelike/Assets/Scripts/BoardManager.cs
@@ -18,7 +18,7 @@ public class BoardManager : MonoBehaviour {
         }
     }
 
-    public int collums = 8;
+    public int columns = 8;
     public int rows = 8;
     public Count wallCount = new Count(5, 9);
     public Count foodCount = new Count(1, 5);
@@ -28,6 +28,22 @@ public class BoardManager : MonoBehaviour {
     public GameObject[] foodTiles;
     public GameObject[] enemyTiles;
     public GameObject[] outerwallTiles;
+
+    private Transform boardHolder;
+    private List<Vector3> gridPositions = new List<Vector3>();
+
+    void InitialiseList()
+    {
+        gridPositions.Clear();
+
+        for (int x = 1; x < columns - 1; x++)
+        {
+            for (int y = 1; y < rows - 1; y++)
+            {
+                gridPositions.Add(new Vector3(x, y, 0f));
+            }
+        }
+    }
 
     // Use this for initialization
     void Start () {
