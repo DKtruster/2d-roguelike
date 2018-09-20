41fff13e1652e0bb4afb0bd6f43cceeb6c649b02
Lukas <lukashtxb16@gmail.com>
2018-09-20 13:03:19 +0200

    script update

--- roguelike/Assets/Scripts/BoardManager.cs
+++ roguelike/Assets/Scripts/BoardManager.cs
@@ -1,11 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BoardManager : MonoBehaviour {
+    [Serializable]
+    public class Count
+    {
+        public int minimum;
+        public int maximum;
 
-	// Use this for initialization
-	void Start () {
+        public Count (int min, int max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+    }
+
+    public int collums = 8;
+    public int rows = 8;
+    public Count wallCount = new Count(5, 9);
+    public Count foodCount = new Count(1, 5);
+    public GameObject exit;
+    public GameObject[] floorTiles;
+    public GameObject[] wallTiles;
+    public GameObject[] foodTiles;
+    public GameObject[] enemyTiles;
+    public GameObject[] outerwallTiles;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
